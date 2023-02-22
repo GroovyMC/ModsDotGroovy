fe32053d6bc695b44e6dd567d5257265c5cc36c0
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-02-22 20:15:13 +0000

    New multi-project Gradle setup

---
+++ core/src/main/groovy/io/github/groovymc/modsdotgroovy/plugin/ModsDotGroovyPlugin.groovy
@@ -0,0 +1,73 @@
+package io.github.groovymc.modsdotgroovy.plugin
+
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+import groovyjarjarantlr4.v4.runtime.misc.Nullable
+
+@CompileStatic
+interface ModsDotGroovyPlugin {
+    /**
+     * Byte.MAX_VALUE = highest priority, Byte.MIN_VALUE = lowest priority.
+     * Higher priority plugins will have their actions executed first.
+     * @return the priority of this plugin
+     */
+    default byte getPriority() {
+        return 0
+    }
+
+    /**
+     * The name of the plugin, used for logging and debugging purposes.
+     */
+    default String getName() {
+        final String simpleName = getClass().simpleName
+        return simpleName.endsWith('Plugin') ? simpleName[0..-7] : simpleName
+    }
+
+    /**
+     * The version of the plugin, used for logging and debugging purposes.
+     */
+    default float getVersion() {
+        return 1.00f
+    }
+
+    default void init() {
+        println "[$name] Plugin $name v$version initialized"
+    }
+
+    /**
+     * A generic method that's called when a property is set.
+     * Used as a fallback for when a plugin doesn't implement an explicit setter.
+     * @param stack
+     * @param name
+     * @param value
+     * @return <PluginResult | Tuple2<PluginResult, ?> | Object>
+     *     If you return a Tuple2, the first value must be a PluginResult and the second value is either:
+     *         - The type of the changed value.
+     *         - Another Tuple2 of the stack and the changed value, used for moving the changed value to a different location.
+     *     If you return a PluginResult, it'll be treated as Tuple2<(yourPluginResult), null>.
+     *     If you return an Object, it'll be treated as Tuple2<PluginResult.TRANSFORM, (yourObject)>.
+     *     If you return null or don't return anything (void), it'll be treated as Tuple2<PluginResult.VALIDATE, null>.
+     */
+    @CompileDynamic
+    default def set(final Deque<String> stack, final String name, def value) {
+        return PluginResult.UNHANDLED
+    }
+
+    @CompileDynamic
+    default def onNestEnter(final Deque<String> stack, final String name, Map value) {
+        return PluginResult.UNHANDLED
+    }
+
+    @CompileDynamic
+    default def onNestLeave(final Deque<String> stack, final String name, def value) {
+        return PluginResult.UNHANDLED
+    }
+
+    /**
+     * @return A map of default values to use as a fallback when a property is not set and no other plugin has set it.
+     */
+    @Nullable
+    default Map getDefaults() {
+        return null
+    }
+}
---
+++ gradle-plugin/src/main/groovy/io/github/groovymc/modsdotgroovy/gradle/MDGExtension.groovy
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2022 GroovyMC
+ * SPDX-License-Identifier: MIT
+ */
+
+package io.github.groovymc.modsdotgroovy.gradle
+
+import groovy.transform.CompileStatic
+import groovy.transform.stc.ClosureParams
+import groovy.transform.stc.SimpleType
+import org.gradle.api.Project
+import org.gradle.api.provider.ListProperty
+import org.gradle.api.provider.MapProperty
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.SourceSet
+
+@CompileStatic
+abstract class MDGExtension {
+    public static final String NAME = 'modsDotGroovy'
+
+    abstract Property<String> getDslVersion()
+    abstract Property<Boolean> getAutomaticConfiguration()
+    abstract ListProperty<Platform> getPlatforms()
+    abstract Property<SourceSet> getSource()
+    abstract Property<MultiloaderConfiguration> getMultiloader()
+    abstract MapProperty<String, Object> getArguments()
+    abstract ListProperty<String> getCatalogs()
+
+    protected final Project project
+
+    MDGExtension(final Project project) {
+        this.project = project
+        automaticConfiguration.set(true)
+        platforms.set([Platform.FORGE])
+        arguments.set([:])
+        catalogs.set(['libs'])
+    }
+
+    String frontendDsl(final String dep = "io.github.groovymc.modsdotgroovy:dsl:${getDslVersion().getOrElse('2.0.0')}") {
+        return dep
+    }
+
+    List<String> mdgPlugins(final List<String> deps = ["io.github.groovymc.modsdotgroovy:plugins:2.0.0"]) {
+        return deps
+    }
+
+    void platform(final String name) {
+        this.setPlatform(name)
+    }
+
+    void setPlatform(final String name) {
+        this.platforms.set([Platform.byName(name)])
+    }
+
+    void setPlatform(final Platform platform) {
+        this.platforms.set([platform])
+    }
+
+    void platforms(final List<String> platforms) {
+        this.platforms.set(platforms.collect {Platform.byName(it)})
+    }
+
+    void platforms(final String[] platforms) {
+        this.platforms(Arrays.asList(platforms))
+    }
+
+    void multiloader(@DelegatesTo(value = MultiloaderConfiguration, strategy = Closure.DELEGATE_FIRST)
+                     @ClosureParams(value = SimpleType, options = 'io.github.groovymc.modsdotgroovy.MDGExtension$MultiloaderConfiguration') final Closure closure) {
+        final conf = new MultiloaderConfiguration()
+        closure.delegate = conf
+        closure.resolveStrategy = Closure.DELEGATE_FIRST
+        closure.call(conf)
+        multiloader.set(conf)
+    }
+
+    enum Platform {
+        QUILT {
+            @Override
+            String toString() {
+                return 'quilt'
+            }
+        },
+        FORGE {
+            @Override
+            String toString() {
+                return 'forge'
+            }
+        },
+        MULTILOADER {
+            @Override
+            String toString() {
+                return 'multiloader'
+            }
+        }
+
+        Platform() {}
+
+        static Platform byName(String name) {
+            switch (name.toLowerCase(Locale.ROOT)) {
+                case 'quilt':
+                    return QUILT
+                case 'forge':
+                    return FORGE
+                case 'multiloader':
+                    return MULTILOADER
+                default:
+                    throw new IllegalArgumentException("Unknown project platform: $name")
+            }
+        }
+    }
+
+    static class MultiloaderConfiguration {
+        Project common
+        List<Project> forge = []
+        List<Project> quilt = []
+    }
+}
