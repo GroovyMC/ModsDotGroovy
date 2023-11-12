e45fc12aa0180b548b223a72866b7e771d01bd40
Paint_Ninja <PaintNinja@users.noreply.github.com>
2023-11-12 21:14:30 +0000

    Fix NeoGradle 7 support
    Requires over 30 lines of code vs ~3 on other platforms, but it finally works

--- gradle-plugin/src/main/groovy/org/groovymc/modsdotgroovy/gradle/tasks/GatherNeoForgePlatformDetails.groovy
+++ gradle-plugin/src/main/groovy/org/groovymc/modsdotgroovy/gradle/tasks/GatherNeoForgePlatformDetails.groovy
@@ -1,7 +1,14 @@
 package org.groovymc.modsdotgroovy.gradle.tasks
 
+import groovy.transform.CompileDynamic
 import groovy.transform.CompileStatic
-import org.gradle.api.artifacts.Dependency
+import groovy.transform.Memoized
+import net.neoforged.gradle.common.runs.run.RunImpl
+import net.neoforged.gradle.common.runtime.definition.CommonRuntimeDefinition
+import net.neoforged.gradle.common.util.TaskDependencyUtils
+import net.neoforged.gradle.dsl.common.runs.run.Run
+import net.neoforged.gradle.dsl.common.runtime.spec.Specification
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.tasks.CacheableTask
 import org.gradle.api.tasks.TaskAction
 import org.jetbrains.annotations.Nullable
@@ -16,30 +23,50 @@ abstract class GatherNeoForgePlatformDetails extends AbstractGatherPlatformDetai
 
     @TaskAction
     void run() throws IllegalStateException {
-        @Nullable String minecraftVersion = this.minecraftVersion.getOrNull()
-        @Nullable String platformVersion = this.platformVersion.getOrNull()
+        @Nullable String minecraftVersion = this.minecraftVersion.getOrNull() ?: versions[0]
+        @Nullable String platformVersion = this.platformVersion.getOrNull() ?: versions[1]
 
-        if (minecraftVersion === null || platformVersion === null) {
-            final @Nullable def neoGradleUserdevExtension = project.extensions.findByName('userDev')
-
-            final @Nullable Dependency dep = this.dependencies?.find {group == 'net.neoforged' && name == 'neoforge' }
-            if (dep === null)
-                throw new IllegalStateException("""
-                    Could not find Minecraft dependency in configuration \"${this.configurationName.get()}\" for project \"${project.name}\".
-                    Try manually setting the minecraftVersion and platformVersion properties for this task.
-                """.strip().stripIndent())
-
-            final String[] version = dep.version.split('.', 3)
-            if (version.length === 1)
-                throw new IllegalStateException("""
-                    Could not find Forge version in Minecraft dependency version \"$version\" for project \"${project.name}\".
-                    Try manually setting the minecraftVersion and platformVersion properties for this task.
-                """.strip().stripIndent())
-
-            minecraftVersion ?= "1.${version[0]}.${version[1]}".toString()
-            platformVersion ?= dep.version
-        }
+        if (minecraftVersion === null || platformVersion === null)
+            throw new IllegalStateException("""
+                Could not find Minecraft or NeoForge version in run configs for project \"${project.name}\".
+                Try manually setting the minecraftVersion and platformVersion properties for this task.
+            """.strip().stripIndent())
 
         this.writePlatformDetails(minecraftVersion, platformVersion)
+    }
+
+    @Memoized
+    private @Nullable String[] getVersions() {
+        @Nullable String mcVersion = null
+        @Nullable String neoForgeVersion = null
+
+        final NamedDomainObjectContainer<Run> runs = (NamedDomainObjectContainer<Run>) project.extensions.findByName('runs')
+        for (run in runs) {
+            if (run !instanceof RunImpl) continue
+
+            for (sourceSet in run.modSources.get()) {
+                final @Nullable runtimeDef = (CommonRuntimeDefinition) TaskDependencyUtils.findRuntimeDefinition(project, sourceSet).orElse(null)
+                if (runtimeDef === null) continue
+
+                final Specification spec = runtimeDef.specification
+                mcVersion ?= spec.minecraftVersion
+                neoForgeVersion ?= getForgeVersionFromSpec(spec)
+            }
+        }
+
+        return new String[] { mcVersion, neoForgeVersion }
+    }
+
+    /**
+     * Due to a NeoGradle 7 bug/limitation, we can't depend on the userdev module to do this statically as it'll cause
+     * crashes for users when the versions don't precisely match up.
+     */
+    @CompileDynamic
+    private static @Nullable String getForgeVersionFromSpec(Specification spec) {
+        try {
+            return spec.getForgeVersion()
+        } catch (e) {
+            return null
+        }
     }
 }
