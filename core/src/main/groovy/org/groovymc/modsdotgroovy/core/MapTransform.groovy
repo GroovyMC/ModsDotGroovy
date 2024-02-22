541d022f7b0ccc2c9b6a00c830654668f6bb4115
Paint_Ninja <PaintNinja@users.noreply.github.com>
2024-02-22 20:49:32 +0000

    Make `MapTransform` a record

--- core/src/main/groovy/org/groovymc/modsdotgroovy/core/MapTransform.groovy
+++ core/src/main/groovy/org/groovymc/modsdotgroovy/core/MapTransform.groovy
@@ -5,23 +5,15 @@ import groovy.transform.stc.ClosureParams
 import groovy.transform.stc.FirstParam
 
 @CompileStatic
-final class MapTransform {
-    final Class<?> type
-    final Closure<?> transformer
-
-    private MapTransform(final Class<?> type, final Closure<?> transformer) {
-        this.type = type
-        this.transformer = transformer
-    }
-
+record MapTransform<T>(Class<T> type, @ClosureParams(FirstParam.FirstGenericType) Closure<?> transformer) {
     static <T> MapTransform of(
             final Class<T> type,
-            @ClosureParams(value = FirstParam.FirstGenericType) final Closure<?> transformer
+            @ClosureParams(FirstParam.FirstGenericType) final Closure<?> transformer
     ) {
         return new MapTransform(type, transformer)
     }
 
-    def transform(final Object value) {
+    def transform(final def value) {
         if (type.isInstance(value)) {
             return transformer.call(value)
         }
